b5303190ada9c01b2cceceaf7118fa51f6359386
plvbroker <33899191+plvbroker@users.noreply.github.com>
2020-12-19 18:03:47 +0100

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.scss'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
